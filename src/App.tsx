72f4602fa0e6cbcd62f63210530b5f23f50b9a0c
Samuel Gillet <samuel.p.gillet@gmail.com>
2024-01-08 20:03:03 -0700

    add user geolocation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import './App.css'
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, CircleMarker } from "react-leaflet";
 import { GeoJSON } from 'react-leaflet/GeoJSON'
 import coTrackLineData from './data/CO/tracks.json'
 import coWaypointsData from './data/CO/waypoints.json'
@@ -17,12 +18,29 @@ const showPopUp = (feature: { properties: { name: any; }; }, layer: { bindPopup:
 }
 
 function App() {
+  const [userLocation, setUserLocation] = useState([0,0])
+  const [locationAccuracy, setLocationAccuracy] = useState(0)
+  const [userLocationFound, setUserLocationFound] = useState(false)
   
-  const position = [39.7392, -104.9903];
+  const geoloc_success = (pos: { coords: { latitude: any; longitude: any; accuracy: any; }; }) => {
+    setUserLocation([pos.coords.latitude, pos.coords.longitude])
+    setLocationAccuracy(pos.coords.accuracy);
+    setUserLocationFound(true)
+  }
+  
+  const geoloc_error = (err: { code: number; }) => {
+    if (err.code == 1){
+      alert('Please allow geolocation access.')
+    } else {
+      alert('There was a problem getting current location.')
+    }
+  }
+  
+  navigator.geolocation.watchPosition(geoloc_success, geoloc_error);
 
   return (
     <MapContainer
-      center={position}
+      center={[39.7392, -104.9903]}
       zoom={7}
       scrollWheelZoom={true}
       style={{ minHeight: "100vh", minWidth: "100vw" }}
@@ -31,6 +49,15 @@ function App() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {userLocationFound && <Marker position={userLocation}>
+        <Popup>
+          You are here.
+        </Popup>
+      </Marker>}
+      {userLocationFound && 
+        <CircleMarker center={userLocation} radius={locationAccuracy}/>
+      }
+
       <GeoJSON data={coTrackLineData.features} onEachFeature={showPopUp}/>
       <GeoJSON data={coWaypointsData.features} onEachFeature={showPopUp}/>
     </MapContainer>
